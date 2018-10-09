9be16beb2e4260175f8b7b878f9f686c49ab1c11
Reed, Chad <Chad.Reed@ncr.com>
2018-10-09 10:37:50 -0400

    Adding basic kafka setup needed for local development

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,6 +1,10 @@
 package hello;
 
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        kafkaTemplate.sendDefault(UUID.randomUUID().toString(), name);
+
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
